52dac8dc582e2b3e8de1b1c1417776d305f69ff4
ThanaphonSt <specialoei@hotmail.com>
2017-01-29 21:00:54 +0700

    in put template to map.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,13 +6,13 @@
 
     <title>busproject</title>
 
-     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+  
 	<!--Import Google Icon Font-->
 	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<!--Import materialize.css-->
 	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-	
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
@@ -202,11 +202,12 @@
     <main>
     @yield('content')
     </main>
-    <!--Import jQuery before materialize.js-->
+
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+
+    <script type="text/javascript" src="js/materialize.min.js"></script>
     <script type="text/javascript">
         $(".button-collapse").sideNav();
         $('.collapsible').collapsible({accordion : false});
