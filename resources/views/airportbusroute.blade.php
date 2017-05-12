5da9a840dc6f571be1e2c5322d860d4084739837
iidxmania <tonlnwforever@gmail.com>
2017-05-12 20:06:43 +0700

    finished ?

--- resources/views/airportbusroute.blade.php
+++ resources/views/airportbusroute.blade.php
@@ -20,8 +20,10 @@
 @section('content')
     <div id="map"></div>
         <script>
+        var markers = [];
+        var map;
               function initMap(){
-                var map = new google.maps.Map(document.getElementById('map'), {
+                    map = new google.maps.Map(document.getElementById('map'), {
                     zoom: 13,
                     center: {lat: 7.883135, lng: 98.387156},
                     mapTypeId: 'roadmap'
@@ -74,6 +76,94 @@
                   });
                   airport.setMap(map);
               }
+
+              // Adds a marker to the map and push to the array.
+      function addMarker(location) {
+        var marker = new google.maps.Marker({
+          position: location,
+          // icon: iconBase,
+          icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 10,
+                fillColor: 'red',
+                fillOpacity: 1,
+                strokeColor: 'white',
+                strokeWeight: 3
+            },
+          map: map
+        });
+        markers.push(marker);
+      }
+
+      // Sets the map on all markers in the array.
+      function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+
+      // Removes the markers from the map, but keeps them in the array.
+      function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+      // Shows any markers currently in the array.
+      function showMarkers() {
+        setMapOnAll(map);
+
+      setInterval(deleteMarkers, 5000);
+      }
+
+      // Deletes all markers in the array by removing references to them.
+      var latitude;
+      var longitude;
+      // var results;
+      var no1 = [];
+      var no2 = [];
+
+      function deleteMarkers() {
+        $.ajax({url: "http://128.199.195.185/api/v2/givepos3air", success: function(result){
+            $.each(result, function(index, result) {
+              // console.log(result);
+              no1.push({lat: Number(result.lat), lng: Number(result.lon)});
+            });
+            // console.log(result);
+            latitude = result.lat;
+            longitude = result.lon;
+            // results = result;
+        }});
+        result = {lat: Number(latitude), lng: Number(longitude)};
+        clearMarkers();
+        // addMarker({lat: Number(latitude), lng: Number(longitude)});
+        // addMarker({lat: 7.8936129, lng: 98.3531696});
+        for (var i in no1) {
+          // addMarker(no1[i]);
+         var markerPic = new google.maps.Marker({
+         position: new google.maps.LatLng(no1[i].lat, no1[i].lng),
+         icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 10,
+                fillColor: '#D1BB00',
+                fillOpacity: 1,
+                strokeColor: 'white',
+                strokeWeight: 3
+            },
+         map: map
+    });
+    markers.push(markerPic);
+
+    google.maps.event.addListener(markerPic, 'click', (function(markerPic, i) {
+         return function() {
+             infowindow.setContent(locations[i][0]);
+             infowindow.open(map, markerPic);
+         }
+    })(markerPic, i));
+        }
+
+        console.log(no1[1]);
+        no1 = [];
+      }
+showMarkers();
         </script>
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTT9UOlmNjSStQdUc0GcDXa2cfZG4EdB4&callback=initMap">
